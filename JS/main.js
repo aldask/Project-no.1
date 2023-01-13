37eb32f9f60187587de09aa9c569177feb92217e
Aldas <80693733+aldask@users.noreply.github.com>
2023-01-14 00:50:25 +0200

    first js function

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,9 @@ let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 context.fillStyle = "blue";
 context.fillRect(5, 16, 184, 59);
+
+//function no. 1 (show text by a click)
+function showText() { 
+    document.getElementById("magicBox").style.display = "block";; 
+} 
+
