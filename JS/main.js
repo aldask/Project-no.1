9dc905aee344b225cd3901ecb25c14dc153e96f4
Aldas <80693733+aldask@users.noreply.github.com>
2023-01-13 19:43:48 +0200

    canva added

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,7 @@ const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
 
 //canvas script
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
+context.fillStyle = "blue";
+context.fillRect(5, 16, 184, 59);
